a773906b4ab4925183ca64eb45f5af34aa1351ae
an <andrii.tereshchuk@splynx.com>
2020-08-21 19:19:23 +0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Andrii Tereshchuk on 21.08.2020.
+//  Copyright © 2020 Andrii Tereshchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
